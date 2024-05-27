685fa6933ab823cb1ecdd7fbbb034880fb86ad81
Rami Alaidy <ramialaidy.ios@gmail.com>
2024-05-27 17:45:02 +0300

    make it better

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,19 +9,52 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+  
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    // cors middleware should come before default error middleware using `at: .beginning`
+    app.middleware.use(cors, at: .beginning)
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+  
+    
+    if let databaseURL = Environment.get("DATABASE_URL") {
+        
+        var tlsConfig: TLSConfiguration = .makeClientConfiguration()
+        tlsConfig.certificateVerification = .none
+        let nioSSLContext = try NIOSSLContext(configuration: tlsConfig)
 
-    app.migrations.add(CreateTodo())
+        var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
+        postgresConfig.coreConfiguration.tls = .require(nioSSLContext)
+
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "postgres",
+            password: Environment.get("DATABASE_PASSWORD") ?? "r315r199r",
+            database: Environment.get("DATABASE_NAME") ?? "Protofolio",
+            tls: .prefer(try .init(configuration: .clientDefault)))
+        ), as: .psql)
+    }
+    app.migrations.add(CreatePersonInfo())
+    
+    app.migrations.add(CreateExperience())
+    
+    app.migrations.add(CreateProtofolio())
+    
+ 
+    app.logger.logLevel = .debug
+
+ 
+    try await app.autoMigrate()
+
 
     app.views.use(.leaf)
-
 
     // register routes
     try routes(app)
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,13 +2,23 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
-    }
+//    app.get { req async throws in
+//        try await req.view.render("index", ["title": "Hello Vapor!"])
+//    }
+//
+//    app.get("hello") { req async -> String in
+//        "Hello, world!"
+//    }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
-
-    try app.register(collection: TodoController())
+    try app.register(collection: ExperienceController())
+    
+    try app.register(collection: PersonController())
+    
+    try app.register(collection: ProtofolioController())
+    let websiteController = WebsiteController()
+    try app.register(collection: websiteController)
+}
+struct InfoResponse:Content {
+    
+    let request: PersonModel
 }
